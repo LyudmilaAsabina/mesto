53f5e0e7dbc0652f6b363c845b5e4f016ca45149
Lyudmila Asabina <milla24g@mail.ru>
2022-06-09 16:53:32 +0300

    добавление файлов

---
+++ pages/script.js
@@ -0,0 +1,40 @@
+
+const formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('#edit_name');
+let jobInput = formElement.querySelector('#edit_comment');
+let profileAuthor = document.querySelector('.profile__author');
+let profileSubline = document.querySelector('.profile__author-subline');
+const formOpener = document.querySelector('.profile__edit-button');
+const content = document.querySelector('.popup');
+const formCloused = document.querySelector('.popup__close-button')
+
+function popupOpened() {
+    content.classList.add('popup_opened');
+    nameInput.value = profileAuthor.textContent;
+    jobInput.value = profileSubline.textContent;
+}
+formOpener.addEventListener('click', popupOpened);
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    profileAuthor.textContent = nameInput.value;
+    profileSubline.textContent = jobInput.value;
+    popupClosed();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+const formCloser = document.querySelector('.popup__close-button');
+
+function popupClosed() {
+    content.classList.remove('popup_opened');
+}
+
+formCloser.addEventListener('click', popupClosed);
+
+
+
+
+
